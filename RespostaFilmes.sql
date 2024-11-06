b2d643639ca8a325730e6f6e8c0f777911cc4227
Igor Limaverde <138315459+igorin23@users.noreply.github.com>
2024-11-06 16:15:12 -0300

    Create RespostaFilmes.sql
    Respostas do desafio de SQL

---
+++ RespostaFilmes.sql
@@ -0,0 +1,89 @@
+-- 1
+SELECT 
+	Nome, 
+	Ano 
+FROM Filmes
+
+-- 2
+SELECT 
+	Nome, 
+	Ano
+FROM Filmes
+ORDER BY Ano
+
+-- 3
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+-- 4
+SELECT
+	*
+FROM Filmes
+WHERE Ano = 1997
+
+-- 5
+SELECT
+	*
+FROM Filmes
+WHERE Ano > 2000
+
+-- 6
+SELECT
+	*
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+-- 7
+SELECT
+	Ano,
+	COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+-- 8
+SELECT
+	PrimeiroNome,
+	UltimoNome
+FROM Atores
+WHERE Genero = 'M'
+
+-- 9
+SELECT
+	PrimeiroNome,
+	UltimoNome
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- 10
+SELECT
+	F.Nome,
+	G.Genero
+FROM FilmesGenero FG
+INNER JOIN Filmes F  ON FG.IdFilme=F.Id
+INNER JOIN Generos G ON FG.IdGenero = G.Id
+
+-- 11
+SELECT
+	F.Nome,
+	G.Genero
+From FilmesGenero FG
+INNER JOIN Filmes F ON FG.IdFilme=F.Id
+INNER JOIN Generos G ON FG.IdGenero=G.Id
+WHERE G.Genero = 'Mistério'
+
+-- 12
+SELECT
+	F.Nome,
+	A.PrimeiroNome,
+	A.UltimoNome,
+	EF.Papel
+From ElencoFilme EF
+INNER JOIN Filmes F ON EF.IdFilme=F.Id
+INNER JOIN Atores A ON EF.IdAtor=A.Id
